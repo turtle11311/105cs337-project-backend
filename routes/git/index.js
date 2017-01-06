6455cb62df3bc78b2ef4cacde71bd7e007f102d2
TurtleBee <turtle11311@gmail.com>
2017-01-06 21:42:15 +0800

    Can handle /apis/git/clone/:reponame

--- routes/git/index.js
+++ routes/git/index.js
@@ -23,6 +23,19 @@ router.post('/:initrepo/init', (req, res) => {
   });
 });
 
+router.post('/clone/:reponame([-a-zA-Z0-9_]+)', (req, res) => {
+  nodegit.Clone(req.body.url,
+                path.join(config.rootDir, 'Repositories', req.params.reponame))
+  .catch((error) => {
+    console.log(error);
+    res.status(406).send(`${error}`);
+  })
+  .then((repo) => {
+    console.log(`${repo}`);
+    res.send(`clone ${req.params.reponame} success`);
+  });
+});
+
 router.param('repo', (req, res, next, repo) => {
   console.log('repo');
   let repoPath = path.join(config.rootDir, 'Repositories', repo);
