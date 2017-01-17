6905fb8c7d0b78c1a846b1689a9a23c41c609161
Wong, Fu-Lian <turtle11311@gmail.com>
2017-01-17 20:49:31 +0800

    Test site

---
+++ public/javascripts/request.js
@@ -0,0 +1,11 @@
+﻿function op() {
+  let method = $('input#reqmethod')[0].value;
+  let url = $('input#requrl')[0].value;
+  let data = JSON.parse($('textarea#reqdata')[0].value);
+  $.ajax({
+    url: url,
+    method: method,
+    data: data,
+    dataType: 'json'
+  });
+}
