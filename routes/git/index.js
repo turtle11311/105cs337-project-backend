35e3af257b1a1c83808478a881288396a1e8b00c
Wong, Fu-Lian <turtle11311@gmail.com>
2017-01-19 10:50:34 +0800

    Can handle commit (But not first commit)

--- routes/git/index.js
+++ routes/git/index.js
@@ -44,6 +44,10 @@ router.param('repo', (req, res, next, repo) => {
     res.status(406).send(`${error}`);
   }).then((repo) => {
     req.repo = repo;
+  }).then(() => {
+    return req.repo.refreshIndex();
+  }).then((indexResult) => {
+    req.repoIndex = indexResult;
     next();
   });
 });
@@ -57,23 +61,36 @@ router.post('/:repo([-a-zA-Z0-9_]+)/add', (req, res) => {
   }
 
   req.repoPromise.then(() => {
-    return req.repo.refreshIndex();
-  }).then((indexResult) => {
-    req.repoIndex = indexResult;
-  }).then(() => {
     return _(files).forEach(fname => req.repoIndex.addByPath(fname));
   }).then((value) => {
     console.log(value);
     return req.repoIndex.write();
-  }).done(() => {
+  }).then(() => {
     return req.repoIndex.writeTree();
+  }).done(() => {
+    res.send(`Add ${files} into Repo ${req.params.repo}`);
   });
-
-  res.send(`Add ${files} into Repo ${req.params.repo}`);
 });
 
 router.post('/:repo([-a-zA-Z0-9_]+)/commit', (req, res) => {
-  // do some thing
+  let massage = req.body.massage;
+  req.repoPromise.then(() => {
+    return req.repoIndex.writeTree();
+  }).then((oidResult) => {
+    req.repoOid = oidResult;
+    return nodegit.Reference.nameToId(req.repo, "HEAD");
+  }).then((head) => {
+    return req.repo.getCommit(head);
+  }).then((parent) => {
+    let author = nodegit.Signature.create("Scott Chacon",
+      "schacon@gmail.com", 123456789, 60);
+    let committer = nodegit.Signature.create("Scott A Chacon",
+      "scott@github.com", 987654321, 90);
+
+    return req.repo.createCommit("HEAD", author, committer, massage, req.repoOid, [parent]);
+  }).done((commitId) => {
+    res.send(`New Commit: ${commitId}, Massage: ${massage}`);
+  });
 });
 
 module.exports = router;
