5c5a43512bcfcca72a73c7bcc84f5dfc5b83617a
Wong, Fu-Lian <turtle11311@gmail.com>
2017-01-22 16:03:42 +0800

    Change list file json format for fancytree

--- routes/fs/folderTraveler.js
+++ routes/fs/folderTraveler.js
@@ -4,15 +4,17 @@ const fs = require('fs-extra');
 const path = require('path');
 
 let folderTraveler = (dir) => {
-  let result = new Object();
+  let result = new Array();
   let files = fs.readdirSync(dir);
   for (let file of files) {
     if (file == '.git') continue;
     let fullpath = path.join(dir, file);
     if (fs.statSync(fullpath).isDirectory()) {
-      result[file] = folderTraveler(fullpath);
+      result.push({
+        title: file, key: file, folder: true, children: folderTraveler(fullpath)
+      });
     } else {
-      result[file] = file;
+      result.push({ title: file, key: file});
     }
   }
   return result;
