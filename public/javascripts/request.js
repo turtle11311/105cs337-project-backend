d5fb4a940c215080de8f3c4c41d5fb99a4d1ade5
Wong, Fu-Lian <turtle11311@gmail.com>
2017-01-28 18:56:38 +0800

    Clone function & reload treeview

--- public/javascripts/request.js
+++ public/javascripts/request.js
@@ -11,6 +11,18 @@
   });
 }
 
+function clone() {
+  var url = 'https://github.com/turtle11311/105cs337-project-backend';
+  var newRepoName = '105cs337-project-backend';
+  $.ajax({
+    method: 'POST',
+    url: `/apis/git/cloneto/${newRepoName}`,
+    contentType: 'application/json',
+    data: JSON.stringify({ url: url }),
+    dataType: 'json'
+  });
+}
+
 function save(){
   var content = ace.edit('editor').getValue();
   console.log(content);
--- public/javascripts/treeview.js
+++ public/javascripts/treeview.js
@@ -7,30 +7,33 @@
   return res ? res.name : 'plain_text';
 };
 
-
-$(() => {
-  // Create the tree inside the <div id="tree"> element.
-  $('#tree').fancytree({
-    source: {
-      url: `/apis/fs/ls/${repo}`
-    },
-    checkbox: true,
-    activate: function (event, data) {
-      // A node was activated: display its title:
-      var node = data.node;
-      if (node.folder) return;
-      $.ajax({
-        method: 'GET',
-        url: `/apis/fs/${repo}/${node.key}`,
-        success: (data, textStatus, jqXHR) => {
-          nowFile = node.key;
-          var editor = ace.edit('editor');
-          editor.$blockScrolling = Infinity;
-          editor.setValue(data);
-          editor.clearSelection();
-          editor.session.setMode(`ace/mode/${filetype(node.key)}`);
-        }
-      });
-    }
+function loadTreeview() {
+  $(() => {
+    $('#tree').remove();
+    $('div.treeView').append($('<div></div>').attr('id', 'tree'));
+    // Create the tree inside the <div id="tree"> element.
+    $('#tree').fancytree({
+      source: {
+        url: `/apis/fs/ls/${repo}`
+      },
+      checkbox: true,
+      activate: function (event, data) {
+        // A node was activated: display its title:
+        var node = data.node;
+        if (node.folder) return;
+        $.ajax({
+          method: 'GET',
+          url: `/apis/fs/${repo}/${node.key}`,
+          success: (data, textStatus, jqXHR) => {
+            nowFile = node.key;
+            var editor = ace.edit('editor');
+            editor.$blockScrolling = Infinity;
+            editor.setValue(data);
+            editor.clearSelection();
+            editor.session.setMode(`ace/mode/${filetype(node.key)}`);
+          }
+        });
+      }
+    });
   });
-});
+}
